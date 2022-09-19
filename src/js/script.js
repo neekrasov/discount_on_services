3479631731a6dbe03faff17b5788149907f0cda5
neekrasov <neekrasov@gmail.com>
2022-09-19 15:55:11 +0300

    added feedback and registration form for the 2nd lab, without refactoring

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,18 @@
 const hamburger = document.querySelector('.nav-menu'),
 menu = document.querySelector('.menu'),
 body = document.querySelector('body'),
-menuLines = document.querySelectorAll('.nav-menu__line');
+menuLines = document.querySelectorAll('.nav-menu__line'),
+createAccount = document.querySelector('.login__create a'),
+signUp = document.querySelector('.sign-up'),
+closeButtonSignUp = document.querySelector(".sign-up__close"),
+support = document.querySelector(".support"),
+supportLink = document.querySelector(".support__link"),
+supportClose = document.querySelector(".support__close");
+
 
 const loginButton = document.querySelector("nav .button_login"),
     login = document.querySelector('.login'),
-    closeButton = document.querySelector('.login__close');
+    closeButtonLogin = document.querySelector('.login__close');
 
 hamburger.addEventListener('click', ()=>{
     body.classList.toggle("overflow-down");
@@ -20,7 +27,28 @@ loginButton.addEventListener('click', ()=>{
     login.classList.add('active');
 });
 
-closeButton.addEventListener('click', ()=>{
+closeButtonLogin.addEventListener('click', ()=>{
     body.classList.remove('overflow-down');
     login.classList.remove('active');
+});
+
+createAccount.addEventListener('click', (e) => {
+    e.preventDefault();
+    signUp.classList.add('active');
+    login.classList.remove('active');
+});
+
+closeButtonSignUp.addEventListener('click', ()=>{
+    signUp.classList.remove('active');
+    login.classList.add('active');
+});
+
+supportLink.addEventListener("click", ()=>{
+    body.classList.add('overflow-down');
+    support.classList.add("active");
+});
+
+supportClose.addEventListener("click",()=> {
+    body.classList.remove('overflow-down');
+    support.classList.remove("active");
 });
